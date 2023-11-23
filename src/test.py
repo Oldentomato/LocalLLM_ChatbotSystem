fe2aac25c569e6fc4114d29e9eb060303790f08d
oldentomto <ender1412@naver.com>
2023-11-23 16:21:49 +0900

    모델 로드 코드 수정

--- src/test.py
+++ src/test.py
@@ -1,4 +1,5 @@
 from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 from langchain.chains import RetrievalQA
 from langchain.document_loaders import PyPDFLoader
 from langchain.callbacks.base import BaseCallbackHandler
@@ -7,19 +8,21 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 import torch
 from peft import PeftModelForCausalLM, get_peft_config
-from transformers import AutoModelForCauslaLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer
 # from .peft2hf import HuggingFaceHugs
+from langchain import HuggingFacePipeline
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
 
-from langchain import PromptTemplate, LLMChain
 
-model_path = "C:\\Users\\유아이네트웍스\\Desktop\\projects\\chatbot_python\\src\\data\\checkpoint-662/"
-adapter_path = "./data/checkpoint-662"
-model_kwargs = {'device': 'cpu'}
+model_path = "/prj/src/data/7b-hf"
+adapter_path = "/prj/src/out/model/llama-7-int4-dolly/checkpoint-1986/"
+model_kwargs = {'device': 'cuda'}
 encode_kwargs = {'normalize_embeddings': True}
 
 def read_pdfs(pdf_files):
     pages = []
-    temp_filepath = os.path.join("./data", pdf_files)
+    temp_filepath = os.path.join("./data/", pdf_files)
     loader = PyPDFLoader(temp_filepath)
     for page in loader.load_and_split():
         pages.append(page)
@@ -34,6 +37,8 @@ def split_pages(pages):
     )
     texts = text_splitter.split_documents(pages)
     return texts
+
+
 
 class StreamHandler(BaseCallbackHandler):
     def __init__(self, initial_text="", update_interval=2):
@@ -50,51 +55,59 @@ class StreamHandler(BaseCallbackHandler):
             print(self.token_buffer)
             self.token_buffer = []
 
+#"/prj/src/out/peft_hf"
+#"beomi/llama-2-ko-7b"
+#"/prj/src/data/7b-hf"
+model = "beomi/llama-2-ko-7b"
 #peft에서 huggingface로 변환하는 작업
-m = AutoModelForCauslaLM.from_pretrained(
-    model_path,
-    torch_dtype=torch.bfloat16,
-    device_map={"": 0}
+# m = AutoModelForCausalLM.from_pretrained(
+#     model_path,
+#     torch_dtype=torch.bfloat16,
+#     device_map={"": 0}
+# )
+# m.eval()
+# m = PeftModelForCausalLM.from_pretrained(m, adapter_path)
+# m = m.merge_and_unload()
+tok = AutoTokenizer.from_pretrained(model)
+tok.bos_token_id = 1
+
+# m.save_pretrained("./out/peft_hf") #여기서 모델만 잘 저장된다면 아래들은 문제없음
+
+
+hf_model = HuggingFacePipeline.from_model_id(
+    model,
+    task="text-generation",
+    model_kwargs={"temperature":1, "max_length":4096, "torch_dtype":torch.float16},
+    device=0
 )
-m = PeftModelForCausalLM.from_pretrained(m, adapter_path)
-m = m.merge_and_unload()
 
-m.save_pretrained("../out/peft_hf") #여기서 모델만 잘 저장된다면 아래들은 문제없음
-
-
-# hf_model = HuggingFaceHugs(model=m, tokenizer=tokenizer)
 
 stream_handler = StreamHandler()
 
-#for test
-template = """ Hey llama, you like to eat quinoa. Whatever question I ask you, you reply with "Waffles, waffles, waffles!".
- Question: {input} Answer: """
-prompt = PromptTemplate(template=template, input_variables=["input"])
-chain = LLMChain(prompt=prompt, llm=hf_model)
+embeddings = HuggingFaceEmbeddings(model_name=model, model_kwargs=model_kwargs, encode_kwargs=encode_kwargs)
+pages = read_pdfs("SAMPLE.pdf")
 
-chain("Who is Princess Momo?")
-#################################################
+# if tok.pad_token is None:
+#     tok.add_special_tokens({'pad_token': '[PAD]'})
+# embeddings.resize_token_embeddings(len(tok))
 
-# embeddings = HuggingFaceEmbeddings(model_name=model_path, model_kwargs=model_kwargs, encode_kwargs=encode_kwargs)
-# pages = read_pdfs("pdf_files.pdf")
+# Split
+texts = split_pages(pages)
 
-# # Split
-# texts = split_pages(pages)
+persist_directory = None  # "./data_store"
+db = Chroma.from_documents( #chromadb 임베딩된 텍스트 데이터들을 효율적으로 저장하기위한 모듈
+    documents=texts,
+    embedding=embeddings,
+    persist_directory=persist_directory)
 
-# persist_directory = None  # "./data_store"
-# db = Chroma.from_documents( #chromadb 임베딩된 텍스트 데이터들을 효율적으로 저장하기위한 모듈
-#     documents=texts,
-#     embedding=embeddings,
-#     persist_directory=persist_directory)
+# Set retriever and LLM
+retriever = db.as_retriever()
 
-# # Set retriever and LLM
-# retriever = db.as_retriever()
+#여기가 문제임
+qa_chain = RetrievalQA.from_chain_type(
+    llm=hf_model,
+    chain_type="stuff",
+    retriever=retriever,
+    return_source_documents=True)
 
-
-# qa_chain = RetrievalQA.from_chain_type(
-#     llm=llm,
-#     chain_type="stuff",
-#     retriever=retriever,
-#     return_source_documents=True)
-
-# response = qa_chain("user_input")
+response = qa_chain("이 pdf를 정리해줘")
