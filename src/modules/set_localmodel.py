254443b6e689403ee3492eb5e15abb7e67cdd988
Oldentomato <ender1412@naver.com>
2023-12-07 11:10:13 +0900

    pipeline내용 수정

--- src/modules/set_localmodel.py
+++ src/modules/set_localmodel.py
@@ -164,11 +164,6 @@ class Set_LocalModel:
             print(f"error_msg: {e}")
             return False, e
 
-        # 지금까지의 대화내역을 사용하여 마지막에 질문에 답합니다. 대화내역은 아래와 같습니다.\n
-        # {chat_history}\n
-        # 사전 지식이 아닌 상황 정보가 주어지면 질문에 대답해주세요. 친근하게 대답해도 되지만 지나치게 수다를 떨면 안 됩니다. 상황 정보는 아래에 있습니다.\n
-        # {context}\n
-
     def __set_prompt(self):
         prompt_template = """당신은 문서검색 지원 에이전트입니다.\n
         요약 정보를 제공하면 그 정보를 활용하여 대답해주세요. 요약 정보는 아래에 있습니다.\n
@@ -176,7 +171,7 @@ class Set_LocalModel:
         대화 내역을 활용하여 추가적으로 답변을 생성해도 됩니다. 대화 내역은 아래와 같습니다.\n
         {chat_history}
         답을 모르면 모른다고만 하고 답을 만들려고 하지 마세요. 같은 말은 반복하지 마세요.\n
-        참고된 pdf의 페이지가 없다면 없다고 답하세요.\n
+        참고된 pdf의 내용이 없다면, 없다고 답하세요.\n
         """
         system_prompt = PromptTemplate(
             template=prompt_template,
@@ -206,17 +201,36 @@ class Set_LocalModel:
             db.get() 
 
 
-            streamer = TextStreamer(g=g, tokenizer=self.tokenizer, skip_prompt=True, Timeout=3)
+            streamer = TextStreamer(g=g, tokenizer=self.tokenizer, skip_prompt=True)
 
             pipe = pipeline(
-                "text-generation", model=self.pre_model, repetition_penalty=1.1, tokenizer=self.tokenizer, return_full_text = False, max_new_tokens=200, streamer=streamer
+                "text-generation", 
+                model=self.pre_model, 
+                repetition_penalty=1.5, #높을수록 반복성을 방지시킴
+                tokenizer=self.tokenizer, 
+                return_full_text = False, 
+                max_new_tokens=200, #max_length는 프롬프트를 포함한 최대길이를 지정, max_new_tokens는 프롬프트를 제외한 최대길이를 지정함
+                streamer=streamer,
             )
-            hf_model = HuggingFacePipeline(pipeline=pipe)
+            res = pipe( #__call__
+                do_sample = True, # False로 해두면 Greedy Search만 진행되어 같은 결과만 생성하게 된다.
+                no_repeat_ngram_size=2, #생성시간이 숫자에 비례해서 올라가지만 문장 생성시에 반복되는 경우를 줄일 수 있다.
+                temperature=0.2, # 낮을수록 가장 확률이 높은 토큰을 선택하게됨. 높을수록 더 창의적인 답변이 나오게됨 (낮음: 사실적, 높음: 창의적)
+                #top_k와 top_p는 greedy가 False(do_sample이 True)인 경우에 활용된다.
+                top_k=0, #확률이 높은 순서대로 k번째까지 높은 단어에 대해 필터링한다. 값이 높을수록 무작위 샘플 방식에 가까워지고 낮을수록 탐욕 방식에 가까워진다.
+                top_p=0.95, #여기서 top_k를 0으로두고 top_p의 값만 할당해주면 뉴클러스 샘플링이라고 하게 된다. 확률이 가장 높은 순으로 후보 단어들을 더했을 때 top_p가 되는단어 집합에 대해 샘플링한다.
+                
+                #즉, greedy를 True로 하면 항상 확률이 높은 단어만 선택하기때문에 편향적이고 일관된 문장만 출력하게되고, 경우에 따라 반복되는 단어가 나올수 있고,
+                #greedy를 False로 하면 top_k와top_p의 샘플링 방식을 통해 좀 더 자연스럽고 다양한 문장들을 출력하게 된다.
+
+
+            )
+            hf_model = HuggingFacePipeline(pipeline=res)
 
             memory = ConversationBufferMemory(memory_key="chat_history", human_prefix="question", ai_prefix="answer", return_messages=True)
 
             # Set retriever and LLM
-            retriever = db.as_retriever(search_type="similarity", search_kwargs={"k": 3})
+            retriever = db.as_retriever(search_type="similarity", search_kwargs={"k": 3}) # 3개의 답변을 생성하고 유사도를 기준으로 가장 높은 점수를 최종답변으로 도출함
 
             #compression_retriever
             print("qa_chain load")
